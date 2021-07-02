928225e0742d08168001232b6bd60327280e6b03
cc01cc <cc01cc@foxmail.com>
2021-07-02 22:10:23 +0800

    test

--- lab/p02-helloworld/helloworld.cpp
+++ lab/p02-helloworld/helloworld.cpp
@@ -1,22 +1,29 @@
-#include "helloworld.h"
-// Register the window class.
-
 const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
 WNDCLASS wc = {};
 
-// 指向应用程序定义的函数
 wc.lpfnWndProc = WindowProc;
-// 应用程序实例的句柄
 wc.hInstance = hInstance;
-// 标识窗口类的字符串
 wc.lpszClassName = CLASS_NAME;
 
 RegisterClass(&wc);
 
 HWND hwnd = CreateWindowEx(
-	0,
-	CLASS_NAME,
-	L"Learn to Program Window",
-	WS_OVERLAPPENDWINDOW
-)
+    0,                              // Optional window styles.
+    CLASS_NAME,                     // Window class
+    L"Learn to Program Windows",    // Window text
+    WS_OVERLAPPEDWINDOW,            // Window style
+
+    // Size and position
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+    NULL,       // Parent window    
+    NULL,       // Menu
+    hInstance,  // Instance handle
+    NULL        // Additional application data
+);
+
+if (hwnd == NULL)
+{
+    return 0;
+}
